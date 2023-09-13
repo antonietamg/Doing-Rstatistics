d983e77b5aa75376b25e4bae74c4d3f6512161cf
Antonieta Martínez-Guerrero <62116253+antonietamg@users.noreply.github.com>
2023-09-13 16:38:26 -0600

    Add files via upload

---
+++ Intro RStudio/Ejercicios Intro RStudio.R
@@ -0,0 +1,37 @@
+##################################################################
+############## EJERCICIOS RSTUDIO ################################
+##################################################################
+#### POR: ANTONIETA MARTÍNEZ-GUERRERO ############################
+##################################################################
+##################################################################
+
+# 1. Crea una carpeta con tu nombre. Ese será tu working directory. Guarda ahí 
+# todos los archivos que generes durante el ejercicio. 
+# 
+# 2. Abre un editor de texto en tu computadora y escribe una serie de 
+# números en una sola columna. Guarda ese archivo como .txt
+# 
+# 3. Importa a RStudio el archivo .txt que creaste en el paso anterior.
+# 
+# 4. Crea un archivo .csv en Excel, guárdalo e impórtalo a RStudio.
+# 
+# 5. Crea un archivo .xlsx en Excel, guárdalo e impórtalo a RStudio.
+# 
+# 6. Corre el siguiente código para crear una gráfica y expórtala a tu working directory.
+# 
+# x = c(1, 2, 3, 4, 5, 6, 7, 8, 9)
+# plot(x)
+# 
+# 7. Haz un screenshot de tu pantalla de RStudio y guarda la imagen en tu folder de trabajo.
+# (Para este paso utiliza tu método tradicional para hacer capturas de pantalla)
+# 
+# 8. Guarda en tu working directory (tu folder de trabajo)tu Global 
+# Environment con el nombre "GEnv.Rdata".
+# 
+# 9. Haz "fork" al repositorio Doing-Rstatistics: https://github.com/antonietamg/Doing-Rstatistics
+# 
+# 10. Añade tu carpeta (donde estarán todos los archivos de este ejercicio) al repositorio
+# dentro de la carpeta llamada "Intro RStudio".
+# 
+# 11. Haz un "pull-request" al upstream.
+
