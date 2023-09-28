e82d44f7c950c25fd034f83b3f27ee4d2f64d820
Antonieta Martínez-Guerrero <62116253+antonietamg@users.noreply.github.com>
2023-09-27 21:43:33 -0600

    Add files via upload

---
+++ Reporte Descriptivo/Ejercicio mtcars 2/Instrucciones Ejercicio mtcars 2.R
@@ -0,0 +1,39 @@
+# INSTRUCCIONES
+
+# Lee con atencion. Ejecuta las instrucciones y escribe los resultados. Cada
+# pregunta debe venir con su codigo y su respuesta escrita como comentario. 
+# Guarda todos los archivos que generes pues deberás subirlos a Github.
+
+# 1. Visualiza el dataset mtcars y guárdalo como un dataframe llamado "autos" 
+#    en el environment. 
+
+# 2. Crea el resumen estadístico para cada grupo de autos con distinto cilindraje (columna cyl) 
+#    Guarda los resultados en un dataframe y exportalo en un archivo llamado "Cyl.csv"  
+
+# 3. Crea el resumen estadístico para cada grupo de autos con distinto tipo de transmision (columna am).
+#    Guarda los resultados en un dataframe y exportalo en un archivo llamado "Transmision.csv"
+
+# 3. Crea el resumen estadístico para el grupo de autos que pesa mas de 3000 lbs y para el grupo
+#    de autos que pesa menos de 3000 lbs. (columna wt).
+#    Guarda los resultados en un dataframe y exportalo en un archivo llamado "Pesos.csv" 
+
+# 4. Si tuvieras que comparar el gasto de gasolina (mpg) de los autos mas pesados (de mas de
+#    3000 lbs) con el de los menos pesados ¿usarías una prueba paramétrica o una no 
+#    paramétrica? Argumenta tu respuesta con codigo y palabras.
+
+# 4. Si tuvieras que comparar el gasto de gasolina (mpg) de los autos con motor "straight"
+#    versus los autos con motor "shaped" ¿usarías una prueba paramétrica o una no 
+#    paramétrica? Argumenta tu respuesta con codigo y palabras.
+
+# 5. Guarda todo tu global environment en un archivo llamado "Ejercicio2.RData".
+
+#    ************************************************************************************
+#    Al final de este ejercicio debes tener guardados en tu computadora: este archivo con 
+#    las respuestas, los archivos .csv con los resumenes estadisticos descriptivos, y 
+#    el archivo Ejercicio2.RData. 
+
+#    Guarda todos esos archivos en una carpeta con tu nombre y sube esa carpeta a tu fork 
+#    del repositorio de Github DoingRStatistics, dentro de la carpeta  
+#    Reporte Descriptivo > Ejercicio mtcars 2.
+
+#    ************************************************************************************
---
+++ Reporte Descriptivo/Ejercicio mtcars/Instrucciones Ejercicio mtcars.R
@@ -0,0 +1,39 @@
+# INSTRUCCIONES
+
+# Lee con atencion. Ejecuta las instrucciones y escribe los resultados. Cada
+# pregunta debe venir con su codigo y su respuesta escrita como comentario. 
+# Guarda todos los archivos que generes pues deberás subirlos a Github.
+
+# 1. Visualiza el dataset mtcars y guárdalo como un dataframe llamado "autos" 
+#    en el environment. 
+
+# 1. Ve a la pestaña de "Help" y escribe "mtcars" en el buscador. Lee de qué se 
+#    trata este dataset.  
+
+# 3. Grafica con plots tipo línea cada una de las variables. Solamente al ver 
+#    estos plots ¿cual de las variables consideras que tiene una mayor variabilidad?
+#    Escribe tu respuesta y argumenta por qué. (Exporta tus gráficas)
+
+# 4. Grafica las distribuciones de cada variable con hist() y con density().   
+#    ¿Logras identificar algún tipo de distribucion conocida? ¿Cuál o cuáles puedes
+#    puedes identificar? Escribe el nombre de la variable y el tipo de distribución.
+#    Si algunas te parecen desconocidas, escribe al lado "desconocida". (Exporta tus gráficas)  
+
+# 5. Genera el resumen de estadística descriptiva de todas las variables, crea un dataframe 
+#    con los resultados y exportalo como un archivo .csv llamado "Tabla Descriptiva.csv"
+
+# 6. Realiza la prueba de normalidad a cada una de las variables y crea un dataframe 
+#    con el nombre de la variable, el p-value y su categoría "normal" o "no normal".
+#    Exporta el dataframe como un archivo .csv llamado "Prueba Normalidad.csv"
+
+# 7. Guarda todo tu global environment en un archivo llamado "Ejercicio1.RData".
+
+#    ************************************************************************************
+#    Al final de este ejercicio debes tener guardados en tu computadora: este archivo con 
+#    las respuestas, tus gráficas, los archivos TCentral.csv y Prueba Normalidad.csv, y 
+#    el archivo Ejercicio1.RData. 
+
+#    Guarda todos esos archivos en una carpeta con tu nombre y sube esa carpeta a tu fork 
+#    del repositorio de Github DoingRStatistics, dentro de la carpeta llamada Reporte Descriptivo.
+
+#    ************************************************************************************
